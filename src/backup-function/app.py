32ba890528a28a6d7f8741939a8853b4978de980
Audran Dessaint <audran.dessaint@gmail.com>
2024-04-10 16:06:09 +0200

    feat: implement backup lambda

--- src/backup-function/app.py
+++ src/backup-function/app.py
@@ -1,5 +1,17 @@
+import os
+import json
+
+import boto3
+
 def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+
+    bucket_name = os.environ["BUCKET_NAME"]
+
+    for record in event["Records"]:
+        s3.put_object(Bucket=bucket_name, Key=f"{record["messageId"]}.json", Body=record["body"])
+
     return {
-        "StatusCode": 200,
-        "body": "OK"
+        'statusCode': 200,
+        'body': json.dumps('Messages processed and stored in S3')
     }
